440dccfdb39455cdf7cf28eaaa80917873a28d9a
ShaymaAboukaf <martincooper9988@gmail.com>
2024-08-18 15:47:51 +0300

    add validation and animation

--- js/register-form.js
+++ js/register-form.js
@@ -3,13 +3,19 @@ showTab(currentTab);
 
 function showTab(n) {
   const x = document.getElementsByClassName("tab");
-  x[n].style.display = "grid";
+  x[n].style.display = "flex";
 
   fixStepIndicator(n);
 }
 
 function next() {
   const x = document.getElementsByClassName("tab");
+  if (!validateForm()) return false;
+
+  if (currentTab == x.length - 1) {
+    window.location = "index.html";
+    return;
+  }
 
   x[currentTab].style.display = "none";
 
@@ -37,12 +43,52 @@ function fixStepIndicator(n) {
 
 function goToTab(n) {
   const x = document.getElementsByClassName("tab");
-
-  x[currentTab].style.display = "none";
-  currentTab = n;
-  showTab(currentTab);
-  fixStepIndicator(n);
+  // document.querySelector(".my-label").classList.remove("animated-label");
+  document.querySelector(".my-label").classList.add("move-label");
+  document.querySelector(".my-button").classList.add("move-btn");
+  setTimeout(() => {
+    document.querySelector(".my-label").classList.remove("move-label");
+    document.querySelector(".my-button").classList.remove("move-btn");
+    x[currentTab].style.display = "none";
+    currentTab = n;
+    showTab(currentTab);
+    fixStepIndicator(n);
+  }, 300);
 }
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  let tabs,
+    inputs,
+    selects,
+    i,
+    valid = true;
+  tabs = document.getElementsByClassName("tab");
+  inputs = tabs[currentTab].getElementsByTagName("input");
+  selects = tabs[currentTab].querySelectorAll(".select-menu");
+
+  for (i = 0; i < inputs.length; i++) {
+    if (inputs[i].value == "") {
+      inputs[i].className += " invalid";
+      valid = false;
+    }
+  }
+
+  for (i = 0; i < selects.length; i++) {
+    if (selects[i].querySelector(".sBtn-text").dataset.value == "") {
+      selects[i].className += " invalid";
+      valid = false;
+    }
+  }
+
+  return valid;
+}
+
+document.querySelectorAll("input").forEach((input) => {
+  input.addEventListener("input", function () {
+    this.classList.remove("invalid");
+  });
+});
 
 document
   .querySelector("#register-form")
@@ -58,17 +104,26 @@ const optionMenu = document.querySelector(".select-menu"),
 const menus = document.querySelectorAll(".select-btn");
 
 menus.forEach((select) => {
-  select.addEventListener("click", function (e) {
-    e.target.parentNode.classList.toggle("active");
-  });
+  select.addEventListener(
+    "click",
+    function (e) {
+      e.target.parentNode.classList.toggle("active");
+    },
+    true
+  );
 });
 
 options.forEach((option) => {
   option.addEventListener("click", function (e) {
     let selectedOption = e.target.innerText;
     document.querySelector(".active .sBtn-text").innerText = selectedOption;
+    document.querySelector(".active .sBtn-text").dataset.value = selectedOption;
 
     option.parentNode.parentNode.classList.remove("active");
+
+    if (selectedOption !== "") {
+      option.parentNode.parentNode.classList.remove("invalid");
+    }
   });
 });
 
